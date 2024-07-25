05a2665ac3da88472af8a470ab976d33cfbd9f73
TechManTejas <techmantejas@gmail.com>
2024-07-25 20:25:28 +0530

    new github-actions.pub key

--- TejasVaij_DRF_Backend/urls.py
+++ TejasVaij_DRF_Backend/urls.py
@@ -7,7 +7,6 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
